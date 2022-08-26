0745dd9ea22a3041a8f5fc7da76a1408666da5ef
zhuchentong <2037630@163.com>
2022-08-26 16:20:56 +0800

    fix: 更新uniapp版本号

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ preset.content = preset.purge.content
 delete preset.darkMode
 delete preset.purge
 
+preset.corePlugins.splice(
+    preset.corePlugins.findIndex(x => x === 'visibility'),
+    1
+)
+
 /** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
     content: defaultPreset.purge.content,
